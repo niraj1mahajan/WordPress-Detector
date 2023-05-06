0660529d4a8bc2dfbc87b8db3d1a6cc228696ec5
Niraj Mahajan <47189921+niraj1mahajan@users.noreply.github.com>
2023-05-06 22:50:16 +0530

    Update wordpress_detector.py

--- wordpress_detector.py
+++ wordpress_detector.py
@@ -2,36 +2,44 @@ import sys
 import requests
 from termcolor import colored
 
-# check if a website is using WordPress
-def is_wordpress(url):
-    try:
-        response = requests.get(url)
-        if "wp-content" in response.text:
-            return True
-        else:
-            return False
-    except:
-        return False
-
-# read the input file and check each domain
 def check_domains(filename):
-    with open(filename, "r") as file:
-        wordpress_domains = []
-        for line in file:
-            url = line.strip()
-            if is_wordpress(url):
-                print(colored(url, "green") + " is using WordPress")
-                wordpress_domains.append(url)
-            else:
-                print(colored(url, "red") + " is not using WordPress")
-        with open("wordpress_domains.txt", "w") as output_file:
-            for domain in wordpress_domains:
-                output_file.write(domain + "\n")
+    try:
+        with open(filename, "r") as file:
+            domains = file.read().splitlines()
+    except FileNotFoundError:
+        print(colored("Error: File not found", "red"))
+        return
+
+    wordpress_domains = []
+    non_wordpress_domains = []
+
+    for domain in domains:
+        try:
+            response = requests.get(f"http://{domain}")
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+            print(colored(f"Error: Unable to connect to {domain}", "red"))
+            continue
+
+        if "wp-content" in response.text:
+            wordpress_domains.append(domain)
+            print(colored(f"{domain} is using WordPress", "green"))
+        else:
+            non_wordpress_domains.append(domain)
+            print(colored(f"{domain} is not using WordPress", "red"))
+
+    with open("wordpress_domains.txt", "w") as file:
+        file.write("\n".join(wordpress_domains))
+
+    print(colored(f"Total domains checked: {len(domains)}", "blue"))
+    print(colored(f"WordPress sites found: {len(wordpress_domains)}", "green"))
+    print(colored(f"Non-WordPress sites found: {len(non_wordpress_domains)}", "red"))
 
 if __name__ == "__main__":
-    print("WordPress Detector Tool")
-    if len(sys.argv) != 2:
-        print("Usage: python wordpress_detector.py <input_file>")
-        sys.exit(1)
-    filename = sys.argv[1]
-    check_domains(filename)
+    print(colored("WordPress Detector Tool", "yellow"))
+
+    if len(sys.argv) < 2:
+        print(colored("Error: Please specify an input file", "red"))
+        print(colored("Usage: python wordpress_detector.py <filename>", "yellow"))
+    else:
+        filename = sys.argv[1]
+        check_domains(filename)
