561461c524197255d767bb051a9a06d5eb263563
Niraj Mahajan <47189921+niraj1mahajan@users.noreply.github.com>
2023-05-06 22:36:57 +0530

    Update wordpress_detector.py

--- wordpress_detector.py
+++ wordpress_detector.py
@@ -1,35 +1,32 @@
 import sys
-import concurrent.futures
 import requests
+from termcolor import colored
 
-wordpress_strings = ['wp-content', 'wp-admin', 'wp-includes']
-
-if len(sys.argv) != 2:
-    print('\033[91mUsage: python wordpress_detector.py <filename>\033[0m')
-    sys.exit(1)
-
-filename = sys.argv[1]
-
-with open(filename, 'r') as f:
-    domains = f.read().split()
-
-def process_domain(domain):
+# check if a website is using WordPress
+def is_wordpress(url):
     try:
-        response = requests.head('http://' + domain, timeout=5)
-        if any(string in response.content.decode().lower() for string in wordpress_strings):
-            return domain
+        response = requests.get(url)
+        if "wp-content" in response.text:
+            return True
         else:
-            return None
-    except Exception as e:
-        return None
+            return False
+    except:
+        return False
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = list(executor.map(process_domain, domains))
+# read the input file and check each domain
+def check_domains(filename):
+    with open(filename, "r") as file:
+        for line in file:
+            url = line.strip()
+            if is_wordpress(url):
+                print(colored(url, "green") + " is using WordPress")
+            else:
+                print(colored(url, "red") + " is not using WordPress")
 
-wordpress_domains = [domain for domain in results if domain is not None]
-
-with open('wordpress_domains.txt', 'w') as f:
-    for domain in wordpress_domains:
-        f.write(domain + '\n')
-
-print('\n\033[92mWordPress sites:', *wordpress_domains, sep='\n', end='\033[0m\n')
+if __name__ == "__main__":
+    print("WordPress Detector Tool")
+    if len(sys.argv) != 2:
+        print("Usage: python wordpress_detector.py <input_file>")
+        sys.exit(1)
+    filename = sys.argv[1]
+    check_domains(filename)
